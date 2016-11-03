d3d0a349ddaa5b04195f174abd1e53ec574aeb48
Erick Johnson <erick@ejjohnson.org>
2016-11-03 13:53:41 +0100

    basic inventory file control

--- lib/loom/all.rb
+++ lib/loom/all.rb
@@ -3,6 +3,7 @@ require_relative "core_ext"
 require_relative "logger"
 require_relative "config"
 
+require_relative "inventory"
 require_relative "pattern"
 require_relative "context"
 require_relative "dsl"
---
+++ lib/loom/inventory.rb
@@ -0,0 +1,94 @@
+require 'yaml'
+
+module Loom
+  module Inventory
+
+    InvalidHostEntry = Class.new StandardError
+    InventoryFileEntryError = Class.new StandardError
+
+    INVENTORY_FILE_GLOB = "inventory.*"
+
+    class << self
+      def total_inventory
+        config_hostlist = Loom.config.loom_hosts
+        inventory_fileset = InventoryFileSet.new Loom.config.inventory_roots
+
+        hostlist = config_hostlist + inventory_fileset.hostlist
+        InventoryList.new hostlist, inventory_fileset.hostgroups
+      end
+    end
+
+    class InventoryList
+
+      def initialize(hostlist, hostgroups)
+        @hosts = parse_hosts(hostlist).uniq { |h| h.hostname }
+        @hostgroups = hostgroups
+      end
+
+      def parse_hosts(list)
+        list.map do |hoststring|
+          raise InvalidHostEntry, hoststring.class.name unless hoststring.is_a? String
+          host = parse hoststring
+        end
+      end
+
+      def parse(hoststring)
+        SSHKit::Host.new hoststring
+      end
+
+      def hostnames
+        @hosts.map { |h| h.hostname }
+      end
+
+      def group_names
+        @hostgroups.keys
+      end
+    end
+
+    private
+    class InventoryFileSet
+      def initialize(roots)
+        @roots = roots
+
+        inventory_file_paths = @roots.map do |root|
+          search_glob = File.join root, "**", INVENTORY_FILE_GLOB
+          Loom.log.debug { "searching inventory glob: #{search_glob}" }
+
+          Dir.glob search_glob
+        end.flatten.map { |p| File.realpath p}
+
+        @raw_inventories = inventory_file_paths.map do |path|
+          Loom.log.info "loading inventory file #{path}"
+          YAML.load_file path
+        end
+      end
+
+      def hostgroups
+        @raw_inventories.reduce({}) do |map, i|
+          i.each do |entry|
+            if entry.is_a? Hash
+              Loom.log.debug "merging groups in #{entry}"
+              map.merge! entry
+            end
+          end
+          map
+        end
+      end
+
+      def hostlist
+        @raw_inventories.map do |i|
+          i.map do |entry|
+            case entry
+            when String
+              entry
+            when Hash
+              entry.values
+            else
+              raise InventoryFileEntryError, "unexpected entry #{entry}"
+            end
+          end
+        end.flatten
+      end
+    end
+  end
+end
