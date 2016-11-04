7400f24f37eaf7366f5a6e73743516a867888458
Erick Johnson <erick@ejjohnson.org>
2016-11-04 22:58:44 +0100

    added a pattern method for DSL like loom files

--- lib/loom/pattern.rb
+++ lib/loom/pattern.rb
@@ -6,9 +6,15 @@ module Loom::Pattern
     class << self
       def included(pattern_mod)
         Loom.log.debug2(self) { "pattern module loaded => #{pattern_mod}" }
+        pattern_mod.extend ClassMethods
       end
     end
 
+    module ClassMethods
+      def pattern(name, &block)
+        define_method name, &block
+      end
+    end
     # DO NOT ADD METHODS TO THIS MODULE!!!
   end
 end
